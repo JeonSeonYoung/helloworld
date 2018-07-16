89baa87ecf6ea9e4870698a4c2cb3d3f238d718a
SoojungChae <naan_ace@naver.com>
2018-07-17 07:31:30 +0900

    Login 라우팅 추가, 파일 변경

--- UI/src/pages/Login.js
+++ UI/src/pages/Login.js
@@ -1,17 +1,13 @@
-// import React, { Component } from 'react';
-// import { ChatCommon } from '../components/ChatCommon';
-// import { Home, About } from 'pages';
-//
-// class Login extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <Common />
-//                 <Route exact path="/" component={Home}/>
-//                 <Route path="/about" component={About}/>
-//             </div>
-//         );
-//     }
-// }
-//
-// export default Login;
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                로그인
+            </div>
+        );
+    }
+}
+
+export default Login;
--- UI/src/resources/utils/Routes.js
+++ UI/src/resources/utils/Routes.js
@@ -4,6 +4,7 @@ import Header from '../../layouts/Header';
 import Footer from '../../layouts/Footer';
 import Main from '../../pages/Main';
 import SearchFilter from "../../pages/SearchFilter";
+import Login from "../../pages/Login";
 import Chat from "../../pages/Chat";
 
 class RouteGroup extends Component {
@@ -11,6 +12,7 @@ class RouteGroup extends Component {
         return (
             <div>
                 <Route exact path="/" component={Header}/>
+                <Route path="/login" component={Login}/>
                 <Route path="/main" component={Main}/>
                 <Route path="/about" component={Footer}/>
                 <Route path="/searchFilter" component={SearchFilter}/>
