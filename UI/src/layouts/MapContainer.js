75efcdb42c4cd8fcdbd8950ebc23a983b08ef95e
ksa <stoneage9@naver.com>
2018-07-25 18:46:09 +0900

    modified map.

--- UI/src/layouts/MapContainer.js
+++ UI/src/layouts/MapContainer.js
@@ -16,10 +16,14 @@ const Map = compose(
         isMarkerShown: false,
         markerPosition: null
     }), {
-            onMapClick: ({ isMarkerShown }) => (e) => ({
-                markerPosition: e.latLng,
-                isMarkerShown: true
-            })
+            onMapClick: ({ isMarkerShown }) => (e) => {
+                console.log(JSON.parse(JSON.stringify(e.latLng)).lat)
+                console.log(JSON.parse(JSON.stringify(e.latLng)).lng)
+                return {
+                    markerPosition: e.latLng,
+                    isMarkerShown: true
+                }
+            }
         }),
     withScriptjs,
     withGoogleMap
@@ -35,10 +39,10 @@ const Map = compose(
              props.id != "create_chat" &&
                 <div style={{ background: `white`, border: `1px solid #ccc`, padding: 15 }}>
                     <h1>Are you sure with this location ?</h1>
-                    <button onClick={updateLocation} style={{ height: 60 }}>
+                    <button onClick={updateLocation} style={{ width: 70, height: 60 }}>
                         OK
                     </button>
-                    <button onClick={cancelLocation} style={{ height: 60 }}>
+                    <button onClick={cancelLocation} style={{ width: 70, height: 60 }}>
                         Cancel
                     </button>
                 </div>
@@ -57,6 +61,7 @@ export default class MapContainer extends React.Component {
             <div style={{ height: '100%' }}>
                 <Map
                     id = {this.props.id}
+                    vLocation = {this.props.vLocation}
                     googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
                     loadingElement={<div style={{ height: `100%` }} />}
                     containerElement={<div style={{ height: `400px` }} />}
