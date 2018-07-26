0d9acce39722083ee4fb1bcf0f32560fbb9104c5
namjaromang <pyj@idiocracy.co.kr>
2018-07-26 21:49:07 +0900

    어느 9:48분에 커밋

---
+++ Chatrooms/www/scripts/Client.js
@@ -0,0 +1,121 @@
+/*Facebook Hackton
+park yong jae
+*/
+
+
+window.onload = function() {
+    var client = new Client();
+    client.init();
+};
+var Client = function() {
+    this.socket = null;
+};
+
+Client.prototype = {
+    init: function() {
+        var that = this;
+        this.socket = io.connect();
+        
+        socket.emit('joinroom',{room:'<%=room%>'});
+                    
+         // get cookies for getting facebook info
+         {
+            var id = getUrlVars()['id'];
+            console.log(id);
+
+            // send to server
+            this.socket.emit('data', { id : id});
+        }
+
+        this.socket.on('connect', function() {
+            document.getElementById('info').textContent = 'get yourself a nickname :)';
+            document.getElementById('nickWrapper').style.display = 'block';
+            document.getElementById('nicknameInput').focus();
+        });
+        this.socket.on('nickExisted', function() {
+            document.getElementById('info').textContent = '!nickname is taken, choose another pls';
+        });
+
+
+        this.socket.on('loginSuccess', function() {
+            document.title = 'hichat | ' + document.getElementById('nicknameInput').value;
+            document.getElementById('loginWrapper').style.display = 'none';
+            document.getElementById('messageInput').focus();
+        });
+
+
+        this.socket.on('error', function(err) {
+            if (document.getElementById('loginWrapper').style.display == 'none') {
+                document.getElementById('status').textContent = '!fail to connect :(';
+            } else {
+                document.getElementById('info').textContent = '!fail to connect :(';
+            }
+        });
+
+
+        this.socket.on('system', function(nickName, count, type) {
+            var msg = nickName + (type == 'login' ? ' joined' : ' left');
+            that._displayNewMsg('system ', msg, 'red');
+            document.getElementById('status').textContent = count + (count > 1 ? ' users' : ' user') + ' online';
+        });
+        this.socket.on('newMsg', function(user, msg, color) {
+            that._displayNewMsg(user, msg, color);
+        });
+
+
+        //닉네임을 넣줘야 로그인을 함
+        document.getElementById('loginBtn').addEventListener('click', function() {
+            var nickName = document.getElementById('nicknameInput').value;
+            if (nickName.trim().length != 0) {
+                that.socket.emit('login', nickName);
+            } else {
+                document.getElementById('nicknameInput').focus();
+            };
+        }, false);
+
+        
+        document.getElementById('nicknameInput').addEventListener('keyup', function(e) {
+            if (e.keyCode == 13) {
+                var nickName = document.getElementById('nicknameInput').value;
+                if (nickName.trim().length != 0) {
+                    that.socket.emit('login', nickName);
+                };
+            };
+        }, false);
+
+        //메세지 보내기
+        document.getElementById('sendBtn').addEventListener('click', function() {
+            var messageInput = document.getElementById('messageInput'),
+                msg = messageInput.value,
+                color = document.getElementById('colorStyle').value;
+            messageInput.value = '';
+            messageInput.focus();
+            if (msg.trim().length != 0) {
+                that.socket.emit('postMsg', msg, color);
+                that._displayNewMsg('me', msg, color);
+                return;
+            };
+        }, false);
+
+        document.getElementById('messageInput').addEventListener('keyup', function(e) {
+            var messageInput = document.getElementById('messageInput'),
+                msg = messageInput.value,
+                color = document.getElementById('colorStyle').value;
+            if (e.keyCode == 13 && msg.trim().length != 0) {
+                messageInput.value = '';
+                that.socket.emit('postMsg', msg, color);
+                that._displayNewMsg('me', msg, color);
+            };
+        }, false);
+
+        //히스토리 클린
+        document.getElementById('clearBtn').addEventListener('click', function() {
+            document.getElementById('historyMsg').innerHTML = '';
+        }, false);
+    },
+        _displayNewMsg: function(user, msg, color) {
+        var container = document.getElementById('historyMsg'),
+            msgToDisplay = document.createElement('p'),
+            date = new Date().toTimeString().substr(0, 8),            
+    }
+};
